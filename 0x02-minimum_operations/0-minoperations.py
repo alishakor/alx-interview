896d16785c1d087807ad6a8cc5d923d358f21b44
alishakor <aliuokeowo@gmail.com>
2023-08-11 03:14:19 +0100

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""The minimum operations coding challenge.
+"""
+
+
+def minOperations(n):
+    """Computes the fewest number of operations needed to result
+    in exactly n H characters.
+	
+	Args:
+		n(int): number of characters
+
+	Returns:
+		operations(int): number of operations
+    """
+    if not isinstance(n, int):
+        return 0
+    operations = 0
+    clipboard = 0
+    prev_h = 1
+    # print('H', end='')
+    while prev_h < n:
+        if clipboard == 0:
+            # copy all and paste
+            clipboard = prev_h
+            prev_h += clipboard
+            operations += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif n - prev_h > 0 and (n - prev_h) % prev_h == 0:
+            # copy all and paste
+            clipboard = prev_h
+            prev_h += clipboard
+            operations += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif clipboard > 0:
+            # paste
+            prev_h += clipboard
+            operations += 1
+            # print('-(01)->{}'.format('H' * done), end='')
+    # print('')
+    return operations
