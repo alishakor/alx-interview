4866410fb80517394bc0a4904a9d40f9e5ab325e
alishakor <aliuokeowo@gmail.com>
2023-08-28 01:21:04 +0100

    new solution

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,28 +1,27 @@
-#!/usr/bin/env python3
-"""UTF-8 validation module"""
+#!/usr/bin/python3
+"""
+UTF-8 Validation
+"""
 
 
-def validUTF8(data):
-    """ method that determines if a given data set represents a
-        valid UTF-8 encoding.
+def validUTF8(data) -> bool:
     """
-    bytes_left = 0
-
-    for num in data:
-        if bytes_left == 0:
-            if num >> 7 == 0b0:   # 1-byte character
-                bytes_left = 0
-            elif num >> 5 == 0b110:  # 2-byte character
-                bytes_left = 1
-            elif num >> 4 == 0b1110:  # 3-byte character
-                bytes_left = 2
-            elif num >> 3 == 0b11110:  # 4-byte character
-                bytes_left = 3
-            else:
+    Returns True if data is a valid UTF-8 encoding, else return False
+    :param data:
+    """
+    num_bytes = 0
+    for byte in data:
+        mask = 1 << 7
+        if not num_bytes:
+            while byte & mask:
+                num_bytes += 1
+                mask >>= 1
+            if not num_bytes:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
-            if num >> 6 != 0b10:  # Continuation byte
+            if byte >> 6 != 0b10:
                 return False
-            bytes_left -= 1
-
-    return bytes_left == 0
+        num_bytes -= 1
+    return num_bytes == 0
